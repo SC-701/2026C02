14e6616c9f031a4b5d35abd677a8dd2b11d5a2aa
Diego <darojasv@outlook.com>
2026-06-15 16:27:45 -0600

    Ajustes

---
+++ Semanas/Semana 02-API Estructura Parte 1/Vehiculo.API/API/Controllers/VehiculoController.cs
@@ -0,0 +1,46 @@
+﻿using Abstracciones.Interfaces.API;
+using Abstracciones.Interfaces.Flujo;
+using Abstracciones.Modelos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculoController : ControllerBase, IVehiculoController
+    {
+        private IVehiculoFlujo _vehiculoFLujo;
+        private ILogger<VehiculoController> _logger;
+
+        public VehiculoController(IVehiculoFlujo vehiculoFLujo, ILogger<VehiculoController> logger)
+        {
+            _vehiculoFLujo = vehiculoFLujo;
+            _logger = logger;
+        }
+
+        public Task<IActionResult> Agregar(VehiculoRequest vehiculo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Editar(Guid Id, VehiculoRequest vehiculo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Eliminar(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Obtener()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Obtener(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Semanas/Semana 02-API Estructura Parte 1/Vehiculo.API/BD/dbo/Stored Procedures/AgregarVehiculo.sql
@@ -0,0 +1,43 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[AgregarVehiculo]
+	-- Add the parameters for the stored procedure here
+	@Id AS uniqueidentifier
+	,@IdModelo AS uniqueidentifier
+	,@Placa AS varchar(max)
+	,@Color AS varchar(max)
+	,@Anio AS int
+	,@Precio AS decimal(18,0)
+	,@CorreoPropietario AS varchar(max)
+	,@TelefonoPropietario AS varchar(max)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+BEGIN TRAN
+    -- Insert statements for procedure here
+	INSERT INTO [dbo].[Vehiculo]
+           ([Id]
+           ,[IdModelo]
+           ,[Placa]
+           ,[Color]
+           ,[Anio]
+           ,[Precio]
+           ,[CorreoPropietario]
+           ,[TelefonoPropietario])
+     VALUES
+           (@Id
+			,@IdModelo
+			,@Placa
+			,@Color
+			,@Anio
+			,@Precio
+			,@CorreoPropietario
+			,@TelefonoPropietario)
+			SELECT @ID
+COMMIT TRAN
+END
